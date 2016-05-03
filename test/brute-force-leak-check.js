5bd0cb024acba36ee107e93eb55691f6f681debb
fivdi <bdscooke@gmail.com>
2016-05-03 22:34:30 +0200

    comment improved

--- test/brute-force-leak-check.js
+++ test/brute-force-leak-check.js
@@ -1,13 +1,13 @@
 'use strict';
 
 /*
- * Create a million epoll instances and use each of them to detect a single
- * event. The goal here is ensure that memory usage doesn't constantly increase
- * over time. Using valgrind might be a better idea here.
+ * Create 1e9 epoll instances and use each of them to detect a single event.
+ * The goal here is ensure that memory usage doesn't constantly increase over
+ * time.
  *
  * This test expects a newline as input on stdin. It polls for events on stdin
  * but doesn't read stdin until it has been notified about the availability of
- * input data 1000000 times.
+ * input data 1e9 times.
  */
 var Epoll = require('../build/Release/epoll').Epoll,
   util = require('./util'),
