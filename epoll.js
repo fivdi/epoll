bdd2944bde7270fed3586103a1d98bae410d2baf
Matthew Villeneuve <matthew.villeneuve@gmail.com>
2019-03-01 11:35:54 -0800

    add simple stub for non-linux installs (#29)

--- epoll.js
+++ epoll.js
@@ -1,2 +1,9 @@
-module.exports = require('bindings')('epoll.node');
-
+module.exports = (() => {
+  const osType = require('os').type();
+  if (osType == 'Linux') {
+    require('bindings')('epoll.node');
+  } else {
+    console.warn(`epoll is built for Linux. Reported OS: ${osType}. Begin mock mode.`);
+    return { Epoll: {} }
+  }
+})()
